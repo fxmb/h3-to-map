93dbbfe4f5ec55a1b4caccce3d20e946af30b824
Franz Bewerunge <39501592+franzbewerunge@users.noreply.github.com>
2021-02-05 17:00:40 +0100

    Update Map.tsx

--- components/Map/Map.tsx
+++ components/Map/Map.tsx
@@ -24,7 +24,7 @@ export default function Map({hexes, center}) {
         scrollWheelZoom={true}
         className="min-h-full">
         <TileLayer
-	    attribution='&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
+	    attribution='&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="https://openstreetmap.org">OpenStreetMap</a> contributors'
         url='https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png'
         />
 
