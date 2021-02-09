ae7620371910ad2b8ac5cdeb0b089e4f9fce7b2c
Franz Bewerunge <franz.bewerunge@tier.app>
2021-02-09 15:49:27 +0100

    Included Segment into Tracking

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,35 +1,52 @@
 import Document, {
-    Html,
-    Head,
-    Main,
-    NextScript,
-    DocumentContext,
-} from "next/document";
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+} from 'next/document';
 
 class MyDocument extends Document {
+  segmentWriteKey = '7eugz6Uhxnk3BarVhk0vWLuv7X6UYVoY';
 
-    render() {
-        return (
-            <Html>
-                <Head>
-                    <link
-                        rel="stylesheet"
-                        href="https://rsms.me/inter/inter.css"
-                    />
-                    <link rel="preconnect" href="https://fonts.gstatic.com" />
-                    <link
-                        href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300;400;500;700&display=swap"
-                        rel="stylesheet"
-                    />
-                    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.0.1/dist/leaflet.css" />
-                </Head>
-                <body>
-                    <Main />
-                    <NextScript />
-                </body>
-            </Html>
-        );
-    }
+  setSegment = () => {
+    return {
+      __html: `
+            !function(){var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","debug","page","once","off","on"];analytics.factory=function(t){return function(){var e=Array.prototype.slice.call(arguments);e.unshift(t);analytics.push(e);return analytics}};for(var t=0;t<analytics.methods.length;t++){var e=analytics.methods[t];analytics[e]=analytics.factory(e)}analytics.load=function(t,e){var n=document.createElement("script");n.type="text/javascript";n.async=!0;n.src="https://cdn.segment.com/analytics.js/v1/"+t+"/analytics.min.js";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(n,a);analytics._loadOptions=e};analytics.SNIPPET_VERSION="4.1.0";
+                    analytics.load(${this.segmentWriteKey});
+                    analytics.page();
+                    console.log('here1');
+            `,
+    };
+  };
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <script
+            dangerouslySetInnerHTML={
+              this.setSegment() /* eslint-disable-line react/no-danger */
+            }
+          />
+          <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300;400;500;700&display=swap"
+            rel="stylesheet"
+          />
+          <link
+            rel="stylesheet"
+            href="https://unpkg.com/leaflet@1.0.1/dist/leaflet.css"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
 
 export default MyDocument;
